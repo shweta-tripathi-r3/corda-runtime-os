05304354c18929e902dc97458bddda9645ee546d
Luca <Debiasi>
2021-05-10 16:49:32 +0100

    CORE-1221: CPK builds.

---
+++ cpk-test/src/main/kotlin/net/corda/cpktest/CPKTest.kt
@@ -0,0 +1,14 @@
+package net.corda.cpktest
+
+class CPKTest {
+
+    init {
+        println("net.corda.cpktest.CPKTest init")
+    }
+
+}
+
+fun main(args: Array<String>) {
+    println("net.corda.cpktest.CPKTest main")
+    CPKTest()
+}
--- goodbyeworld/src/main/kotlin/net/corda/sample/goodbye/GoodbyeWorld.kt
+++ goodbyeworld/src/main/kotlin/net/corda/sample/goodbye/GoodbyeWorld.kt
@@ -1,21 +1,47 @@
 package net.corda.sample.goodbye
 
+import net.corda.install.InstallService
 import net.corda.sample.api.hello.HelloWorld
+import org.osgi.framework.BundleActivator
+import org.osgi.framework.BundleContext
+import org.osgi.service.component.annotations.Activate
+import org.osgi.service.component.annotations.Component
+import org.osgi.service.component.annotations.Deactivate
 import org.osgi.service.component.annotations.Reference
 
-fun main() {
-    // Some bootstrap code goes here
-
-    GoodbyeWorld().callHelloWorld()
-}
-
-class GoodbyeWorld {
+@Component(immediate = true)
+class GoodbyeWorld: BundleActivator {
 
     @Reference
     var helloWorld: HelloWorld? = null
 
+    @Reference
+    var installService: InstallService? = null
+
     fun callHelloWorld() {
         helloWorld?.sayHello() ?: println("We couldn't find hello world!!!")
+
     }
+
+    fun installCpk() {
+        if (installService != null ){
+            println("INSTALL...")
+        } else {
+            println("NO INSTALL!")
+        }
+    }
+
+    @Activate
+    override fun start(context: BundleContext?) {
+        println("net.corda.sample.goodbye.GoodbyeWorld START")
+        callHelloWorld()
+        installCpk()
+    }
+
+    @Deactivate
+    override fun stop(context: BundleContext?) {
+        println("net.corda.sample.goodbye.GoodbyeWorld STOP")
+    }
+
 }
 
