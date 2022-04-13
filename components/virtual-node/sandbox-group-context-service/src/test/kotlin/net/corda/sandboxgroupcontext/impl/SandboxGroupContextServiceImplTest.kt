fd3fe43e35d90fe499abd3a931af68484478a6d5
Barry Lapthorn <barry.lapthorn@r3.com>
2022-04-13 13:46:01 +0100

    CORE-4095 db sandbox/persist entities
    Basic code, with a few unit tests, and the basic OSGI
    integration tests set up as well.
    
    Remove getUniqueObject extension method (fails to work with mocks)
    
    Slight hack to register custom serializers for the entities until
    I figure out how the serializers work.

--- components/virtual-node/sandbox-group-context-service/src/test/kotlin/net/corda/sandboxgroupcontext/impl/SandboxGroupContextServiceImplTest.kt
+++ components/virtual-node/sandbox-group-context-service/src/test/kotlin/net/corda/sandboxgroupcontext/impl/SandboxGroupContextServiceImplTest.kt
@@ -6,7 +6,6 @@ import net.corda.packaging.CPK
 import net.corda.sandboxgroupcontext.SandboxGroupContext
 import net.corda.sandboxgroupcontext.SandboxGroupType
 import net.corda.sandboxgroupcontext.VirtualNodeContext
-import net.corda.sandboxgroupcontext.getUniqueObject
 import net.corda.sandboxgroupcontext.putUniqueObject
 import net.corda.sandboxgroupcontext.service.impl.SandboxGroupContextCache
 import net.corda.sandboxgroupcontext.service.impl.CloseableSandboxGroupContext
