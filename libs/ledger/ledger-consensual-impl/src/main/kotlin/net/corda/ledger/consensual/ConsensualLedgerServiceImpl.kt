72fdc63665428df923de98549aeb018580b4e2cb
Christian Sailer <christian.sailer@r3.com>
2022-06-27 18:30:18 +0100

    Add dependency to service and make existing test still run

--- libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/ConsensualLedgerServiceImpl.kt
+++ libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/ConsensualLedgerServiceImpl.kt
@@ -1,15 +1,67 @@
 package net.corda.ledger.consensual
 
+import net.corda.v5.application.flows.FlowEngine
+import net.corda.v5.application.flows.SubFlow
+import net.corda.v5.application.messaging.FlowSession
+import net.corda.v5.application.messaging.receive
+import net.corda.v5.application.messaging.unwrap
+import net.corda.v5.base.util.contextLogger
 import net.corda.v5.ledger.consensual.ConsensualLedgerService
+import net.corda.v5.ledger.consensual.ConsensualTestRequest
+import net.corda.v5.ledger.consensual.ConsensualTestResponse
 import net.corda.v5.serialization.SingletonSerializeAsToken
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
-import org.osgi.service.component.annotations.ServiceScope.PROTOTYPE
+import org.osgi.service.component.annotations.Reference
+import org.osgi.service.component.annotations.ServiceScope
 
 
-@Component(service = [ConsensualLedgerService::class, SingletonSerializeAsToken::class], scope = PROTOTYPE)
-class ConsensualLedgerServiceImpl @Activate constructor() : ConsensualLedgerService, SingletonSerializeAsToken {
+@Component(service = [ConsensualLedgerService::class, SingletonSerializeAsToken::class], scope = ServiceScope.PROTOTYPE)
+class ConsensualLedgerServiceImpl @Activate constructor(
+    @Reference(service = FlowEngine::class)
+    val flowEngine: FlowEngine
+) : ConsensualLedgerService, SingletonSerializeAsToken {
+
     override fun double(n: Int): Int {
         return n*2
+    }
+
+
+    class DummyPayload
+
+    companion object {
+        val log = contextLogger()
+    }
+
+    class TestFlowImpl(private val session: FlowSession) : SubFlow<String> {
+        override fun call(): String {
+            log.info("SystemFlow test: TestFlowImpl sending dummy")
+            session.send(DummyPayload())
+            log.info("SystemFlow test: TestFlowImpl awaiting reply")
+            return session.receive<String>().unwrap { it }
+        }
+    }
+
+    class TestFlowResponderImpl(private val params: ConsensualTestResponse) : SubFlow<Unit> {
+        override fun call() {
+            log.info("SystemFlow test: TestFlowResponder awaiting dummy")
+            params.session.receive<DummyPayload>()
+            log.info("SystemFlow test: TestFlowResponder sending response")
+            params.session.send(params.createResponse())
+            log.info("SystemFlow test: TestFlowResponder returning")
+        }
+    }
+
+    override fun receiveTestFlow(request: ConsensualTestResponse) {
+        log.info("SystemFlow test: receiveTestFlow called")
+        flowEngine.subFlow(TestFlowResponderImpl(request))
+        log.info("SystemFlow test: receiveTestFlow returning")
+    }
+
+    override fun startTestFlow(request: ConsensualTestRequest): String {
+        log.info("SystemFlow test: startTestFlow called")
+        val result = flowEngine.subFlow(TestFlowImpl(request.session))
+        log.info("SystemFlow test: startTestFlow returning")
+        return result
     }
 }
--- libs/ledger/ledger-consensual-impl/src/test/kotlin/net/corda/ledger/consensual/ConsensualLedgerServiceImplTest.kt
+++ libs/ledger/ledger-consensual-impl/src/test/kotlin/net/corda/ledger/consensual/ConsensualLedgerServiceImplTest.kt
@@ -1,12 +1,14 @@
 package net.corda.ledger.consensual
 
+import net.corda.v5.application.flows.FlowEngine
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
 
 class ConsensualLedgerServiceImplTest {
     @Test
     fun `Test basic behaviour`() {
-        val service = ConsensualLedgerServiceImpl()
+        val service = ConsensualLedgerServiceImpl(mock(FlowEngine::class.java))
         val res = service.double(4)
         assertEquals(8, res)
     }
