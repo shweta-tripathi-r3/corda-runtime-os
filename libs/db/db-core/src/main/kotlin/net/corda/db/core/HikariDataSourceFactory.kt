eb240c46054d62c29c72949fbe0ac9f6e01c6fb6
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2022-08-02 13:44:01 +0100

    Add debug messages to HikariDataSourceFactory

--- libs/db/db-core/src/main/kotlin/net/corda/db/core/HikariDataSourceFactory.kt
+++ libs/db/db-core/src/main/kotlin/net/corda/db/core/HikariDataSourceFactory.kt
@@ -2,19 +2,26 @@ package net.corda.db.core
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import java.util.UUID
 import javax.sql.DataSource
 
 class HikariDataSourceFactory(
 
     private val hikariDataSourceFactory: (c: HikariConfig) -> CloseableDataSource = { c ->
-        DataSourceWrapper(HikariDataSource(c))
+        val uuid = UUID.randomUUID().toString()
+        println("New HikariDataSource - $uuid")
+        DataSourceWrapper(HikariDataSource(c), uuid)
     }
 ) : DataSourceFactory {
     /**
      * [HikariDataSource] wrapper that makes it [CloseableDataSource]
      */
-    private class DataSourceWrapper(private val delegate: HikariDataSource): CloseableDataSource, DataSource by delegate {
-        override fun close() = delegate.close()
+    private class DataSourceWrapper(private val delegate: HikariDataSource, private val uuid: String):
+        CloseableDataSource, DataSource by delegate {
+        override fun close() {
+            println("DataSourceWrapper.close() - $uuid")
+            delegate.close()
+        }
     }
 
     override fun create(
