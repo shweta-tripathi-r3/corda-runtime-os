2811be95cd791675fe58d58f32682f36c218dee1
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2022-08-02 14:08:19 +0100

    Log getConnection and close on the returned connection

--- libs/db/db-core/src/main/kotlin/net/corda/db/core/HikariDataSourceFactory.kt
+++ libs/db/db-core/src/main/kotlin/net/corda/db/core/HikariDataSourceFactory.kt
@@ -2,13 +2,14 @@ package net.corda.db.core
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import java.sql.Connection
 import java.util.UUID
 import javax.sql.DataSource
 
 class HikariDataSourceFactory(
 
     private val hikariDataSourceFactory: (c: HikariConfig) -> CloseableDataSource = { c ->
-        val uuid = UUID.randomUUID().toString()
+        val uuid = getNewUuid()
         println("New HikariDataSource - $uuid")
         DataSourceWrapper(HikariDataSource(c), uuid)
     }
@@ -18,6 +19,33 @@ class HikariDataSourceFactory(
      */
     private class DataSourceWrapper(private val delegate: HikariDataSource, private val uuid: String):
         CloseableDataSource, DataSource by delegate {
+
+        override fun getConnection(username: String?, password: String?): Connection {
+            val uuid = getNewUuid()
+            println("New Hikari connection - $uuid")
+
+            val c = delegate.getConnection(username, password)
+            return object : Connection by c {
+                override fun close() {
+                    println("Close Hikari connection - $uuid")
+                    c.close()
+                }
+            }
+        }
+
+        override fun getConnection(): Connection {
+            val uuid = getNewUuid()
+            println("New Hikari connection - $uuid")
+
+            val c = delegate.connection
+            return object : Connection by c {
+                override fun close() {
+                    println("Close Hikari connection - $uuid")
+                    c.close()
+                }
+            }
+        }
+
         override fun close() {
             println("DataSourceWrapper.close() - $uuid")
             delegate.close()
@@ -42,3 +70,5 @@ class HikariDataSourceFactory(
         return hikariDataSourceFactory(conf)
     }
 }
+
+private fun getNewUuid() = UUID.randomUUID().toString()
