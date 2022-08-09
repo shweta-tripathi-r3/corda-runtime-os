8ed75c7f040780c2ffe46aa60652b93aef7bd65e
Matthew Layton <matthew.layton@live.co.uk>
2022-08-09 21:01:25 +0100

    [WIP] Added UTXO module.

---
+++ libs/ledger/ledger-utxo-impl/src/test/kotlin/net/corda/ledger/utxo/package-info.java
@@ -0,0 +1,9 @@
+/*
+For
+    [<<INITIAL>>]
+      ⇒ osgi.identity: (osgi.identity=net.corda.ledger-consensual-impl)
+          ⇒ [net.corda.ledger-consensual-impl version=5.0.0.0-SNAPSHOT]
+              ⇒ osgi.wiring.package: (osgi.wiring.package=net.corda.ledger.common.impl.transactions)
+
+ */
+package net.corda.ledger.utxo;
