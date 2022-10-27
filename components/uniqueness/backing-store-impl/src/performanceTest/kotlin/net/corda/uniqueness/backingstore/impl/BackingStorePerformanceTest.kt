b651a5adca723bdb41c810d55d58a0797966825a
HJ Kim <hj.kim@r3.com>
2022-10-27 17:14:43 +0100

    CORE-6169: Add a new gradle command.
    * Add a dummy & failing test under performanceTest sub-module.
    * Add a new gradle command with the minimum config.

---
+++ components/uniqueness/backing-store-impl/src/performanceTest/kotlin/net/corda/uniqueness/backingstore/impl/BackingStorePerformanceTest.kt
@@ -0,0 +1,13 @@
+package net.corda.uniqueness.backingstore.impl
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class BackingStorePerformanceTest {
+    @Test
+    fun `test placeholder`() {
+        assertTrue(false)
+    }
+}
