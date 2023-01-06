4f335c7da99281338cbf6f6586d44f1485065eda
Conal Smith <conal.smith@r3.com>
2023-01-06 18:56:44 +0000

    CORE-8610 - bump api

--- libs/virtual-node/cpi-datamodel/src/main/kotlin/net/corda/libs/cpi/datamodel/CpkDbChangeLogAuditEntity.kt
+++ libs/virtual-node/cpi-datamodel/src/main/kotlin/net/corda/libs/cpi/datamodel/CpkDbChangeLogAuditEntity.kt
@@ -46,7 +46,8 @@ data class CpkDbChangeLogAuditKey(
     val filePath: String,
 ) : Serializable
 
-fun changelogAuditEntriesForGivenChangesetIds(entityManager: EntityManager, changesetIds: Set<UUID>): Map<String, List<CpkDbChangeLogAuditEntity>> {
+fun changelogAuditEntriesForGivenChangesetIds(entityManager: EntityManager, changesetIds: Set<UUID>):
+        Map<String, List<CpkDbChangeLogAuditEntity>> {
     return changesetIds.chunked(100) { changesetIdSlice ->
         entityManager.createQuery(
             "FROM ${CpkDbChangeLogAuditEntity::class.simpleName}" +
