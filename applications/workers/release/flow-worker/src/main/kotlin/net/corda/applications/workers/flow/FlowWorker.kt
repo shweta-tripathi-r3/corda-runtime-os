a46c543d8f9d2059a020d4ff489baa3b56fedea8
Ryan Fowler <ryan.fowler@r3.com>
2023-01-16 10:40:16 +0000

    CORE-9199: PoC publishing version info

--- applications/workers/release/flow-worker/src/main/kotlin/net/corda/applications/workers/flow/FlowWorker.kt
+++ applications/workers/release/flow-worker/src/main/kotlin/net/corda/applications/workers/flow/FlowWorker.kt
@@ -2,24 +2,30 @@ package net.corda.applications.workers.flow
 
 import net.corda.applications.workers.workercommon.ApplicationBanner
 import net.corda.applications.workers.workercommon.DefaultWorkerParams
-import net.corda.applications.workers.workercommon.WorkerMonitor
 import net.corda.applications.workers.workercommon.JavaSerialisationFilter
 import net.corda.applications.workers.workercommon.WorkerHelpers.Companion.getBootstrapConfig
 import net.corda.applications.workers.workercommon.WorkerHelpers.Companion.getParams
 import net.corda.applications.workers.workercommon.WorkerHelpers.Companion.loggerStartupInfo
 import net.corda.applications.workers.workercommon.WorkerHelpers.Companion.printHelpOrVersion
 import net.corda.applications.workers.workercommon.WorkerHelpers.Companion.setupMonitor
+import net.corda.applications.workers.workercommon.WorkerMonitor
+import net.corda.data.cluster.WorkerVersion
 import net.corda.libs.configuration.SmartConfigFactoryFactory
 import net.corda.libs.configuration.validation.ConfigurationValidatorFactory
 import net.corda.libs.platform.PlatformInfoProvider
+import net.corda.messaging.api.publisher.config.PublisherConfig
+import net.corda.messaging.api.publisher.factory.PublisherFactory
+import net.corda.messaging.api.records.Record
 import net.corda.osgi.api.Application
 import net.corda.osgi.api.Shutdown
 import net.corda.processors.flow.FlowProcessor
+import net.corda.schema.Schemas.Cluster.Companion.CLUSTER_WORKER_VERSION_TOPIC
 import net.corda.v5.base.util.contextLogger
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
 import picocli.CommandLine.Mixin
+import kotlin.concurrent.thread
 
 /** The worker for handling flows. */
 @Suppress("Unused", "LongParameterList")
@@ -39,11 +45,15 @@ class FlowWorker @Activate constructor(
     val applicationBanner: ApplicationBanner,
     @Reference(service = SmartConfigFactoryFactory::class)
     val smartConfigFactoryFactory: SmartConfigFactoryFactory,
+    @Reference(service = PublisherFactory::class)
+    val publisherFactory: PublisherFactory,
 ) : Application {
 
     private companion object {
         private val logger = contextLogger()
     }
+
+    private var running = false
 
     /** Parses the arguments, then initialises and starts the [flowProcessor]. */
     override fun startup(args: Array<String>) {
@@ -67,11 +77,37 @@ class FlowWorker @Activate constructor(
             params.defaultParams,
             configurationValidatorFactory.createConfigValidator())
 
+        running = true
+        val publisher = publisherFactory.createPublisher(
+            PublisherConfig("", false),
+            config
+        )
+
+        thread {
+            while (running) {
+                publisher.publish(
+                    listOf(
+                        Record(
+                            CLUSTER_WORKER_VERSION_TOPIC,
+                            this::class.java.simpleName,
+                            WorkerVersion(
+                                platformInfoProvider.localWorkerPlatformVersion,
+                                platformInfoProvider.activePlatformVersion,
+                                platformInfoProvider.localWorkerSoftwareVersion
+                            )
+                        )
+                    )
+                )
+                Thread.sleep(1000)
+            }
+        }
+
         flowProcessor.start(config)
     }
 
     override fun shutdown() {
         logger.info("Flow worker stopping.")
+        running = false
         flowProcessor.stop()
         workerMonitor.stop()
     }
