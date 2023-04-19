924931515412f1b9b698ac7036213e6e7e76d784
saurabh.sonawane <saurabh.sonawane@r3.com>
2023-04-19 14:57:48 +0100

    CORE-10426 : Removed InitiatedBy annotation from responder flow.

--- testing/cpbs/test-responder-cordapp/src/main/kotlin/net/cordapp/testing/testflows/FacadeInvocationResponderFlow.kt
+++ testing/cpbs/test-responder-cordapp/src/main/kotlin/net/cordapp/testing/testflows/FacadeInvocationResponderFlow.kt
@@ -1,13 +1,11 @@
 package net.cordapp.testing.testflows
 
-import net.corda.v5.application.flows.InitiatedBy
 import net.corda.v5.application.flows.ResponderFlow
 import net.corda.v5.application.messaging.FlowSession
 import net.corda.v5.base.annotations.Suspendable
 import org.slf4j.LoggerFactory
 //Following protocol name is deliberately used to
 // prove that interop is not using protocol string to start the responder flow
-@InitiatedBy(protocol = "dummy_protocol")
 class FacadeInvocationResponderFlow : ResponderFlow {
     private companion object {
         private val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
