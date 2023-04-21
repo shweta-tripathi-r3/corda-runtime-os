b2cdfc6e4c698e4f3a651a04d8cad404a50fe5f7
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2023-04-21 11:03:30 +0100

    Remove micrometer from depenedencies

--- libs/tracing/src/main/kotlin/net/corda/tracing/TraceThis.kt
+++ libs/tracing/src/main/kotlin/net/corda/tracing/TraceThis.kt
@@ -1,54 +1,57 @@
 package net.corda.tracing
-//
-//import brave.Tracer
-//import brave.Tracing
-//import brave.baggage.BaggageField
-//import brave.baggage.BaggagePropagation
-//import brave.baggage.BaggagePropagationConfig
-//import brave.context.slf4j.MDCScopeDecorator
-//import brave.propagation.B3Propagation
-//import brave.propagation.ThreadLocalCurrentTraceContext
-//import brave.sampler.Sampler
-//import io.micrometer.tracing.CurrentTraceContext
-//import io.micrometer.tracing.brave.bridge.BraveBaggageManager
-//import io.micrometer.tracing.brave.bridge.BraveCurrentTraceContext
-//import io.micrometer.tracing.brave.bridge.BraveTracer
-//import zipkin2.reporter.AsyncReporter
-//import zipkin2.reporter.brave.ZipkinSpanHandler
-//import zipkin2.reporter.urlconnection.URLConnectionSender
-//import java.util.*
-//
-//private val braveTracer: BraveTracer =
-//    run {
-//        val spanHandler = ZipkinSpanHandler
-//            .create(AsyncReporter.create(URLConnectionSender.create("http://localhost:9411/api/v2/spans")))
-//        val braveCurrentTraceContext = ThreadLocalCurrentTraceContext.newBuilder()
-//            .addScopeDecorator(MDCScopeDecorator.get())
-//            .build()
-//        val bridgeContext: CurrentTraceContext = BraveCurrentTraceContext(braveCurrentTraceContext)
-//        val tracing = Tracing.newBuilder().currentTraceContext(braveCurrentTraceContext).supportsJoin(false)
-//            .localServiceName("my-service")
-//            .traceId128Bit(true) // For Baggage to work you need to provide a list of fields to propagate
-//            .propagationFactory(
-//                BaggagePropagation.newFactoryBuilder(B3Propagation.FACTORY)
-//                    .add(
-//                        BaggagePropagationConfig.SingleBaggageField
-//                            .remote(BaggageField.create("from_span_in_scope 1"))
-//                    )
-//                    .add(
-//                        BaggagePropagationConfig.SingleBaggageField
-//                            .remote(BaggageField.create("from_span_in_scope 2"))
-//                    )
-//                    .add(BaggagePropagationConfig.SingleBaggageField.remote(BaggageField.create("from_span")))
-//                    .build()
-//            )
-//            .sampler(Sampler.ALWAYS_SAMPLE).addSpanHandler(spanHandler).build()
-//        val braveTracer1: Tracer = tracing.tracer()
-//        // For Baggage to work you need to provide a list of fields to propagate
-//        BraveTracer(braveTracer1, bridgeContext, BraveBaggageManager())
-//    }
-//
-//
-//fun <T> traceThis(block: () -> T): T {
-//    return block()
-//}
+
+import brave.Tracer
+import brave.Tracing
+import brave.baggage.BaggageField
+import brave.baggage.BaggagePropagation
+import brave.baggage.BaggagePropagationConfig
+import brave.context.slf4j.MDCScopeDecorator
+import brave.handler.SpanHandler
+import brave.jakarta.servlet.TracingFilter
+import brave.kafka.clients.KafkaTracing
+import brave.propagation.B3Propagation
+import brave.propagation.ThreadLocalCurrentTraceContext
+import brave.sampler.Sampler
+import io.javalin.Javalin
+import jakarta.servlet.DispatcherType
+//import org.apache.kafka.clients.producer.KafkaProducer
+//import org.apache.kafka.clients.producer.ProducerRecord
+import org.eclipse.jetty.servlet.FilterHolder
+import org.slf4j.LoggerFactory
+import zipkin2.reporter.AsyncReporter
+import zipkin2.reporter.brave.ZipkinSpanHandler
+import zipkin2.reporter.urlconnection.URLConnectionSender
+import java.util.*
+
+object a {
+    private val spanHandler: SpanHandler = ZipkinSpanHandler
+        .create(AsyncReporter.create(URLConnectionSender.create("http://localhost:9411/api/v2/spans")))
+    private val braveCurrentTraceContext: ThreadLocalCurrentTraceContext = ThreadLocalCurrentTraceContext.newBuilder()
+        .addScopeDecorator(MDCScopeDecorator.get())
+        .build()
+    val tracing: Tracing = Tracing.newBuilder().currentTraceContext(braveCurrentTraceContext).supportsJoin(false)
+        .localServiceName("api-server")
+        .traceId128Bit(true)
+        .propagationFactory(
+            BaggagePropagation.newFactoryBuilder(B3Propagation.FACTORY)
+                .add(BaggagePropagationConfig.SingleBaggageField.remote(BaggageField.create("baggage_1")))
+                .add(BaggagePropagationConfig.SingleBaggageField.remote(BaggageField.create("baggage_2")))
+                .build()
+        )
+        .sampler(Sampler.ALWAYS_SAMPLE).addSpanHandler(spanHandler).build()
+    private val tracer: Tracer = tracing.tracer()
+
+    val kafkaTracing: KafkaTracing = KafkaTracing.newBuilder(tracing)
+        .remoteServiceName("my-broker")
+        .build()
+}
+
+
+fun <T> traceThis(block: () -> T): T {
+    try {
+
+        return block()
+    } finally {
+
+    }
+}
