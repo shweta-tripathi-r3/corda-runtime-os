9ca9ecb538d955b8f6cbe817bdc4062fd4ecf3a0
ShwetaTripathi <shweta.tripathi@r3.com>
2023-05-03 17:25:28 +0530

    Added new no-code plugin

---
+++ tools/plugins/zero-code-cordapp/src/test/kotlin/net/corda/cli/plugins/zerocode/ZeroCodePluginTest.kt
@@ -0,0 +1,24 @@
+package net.corda.cli.plugins.zerocode
+
+import java.nio.file.Files
+import java.nio.file.Path
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+
+class ZeroCodePluginTest {
+    @TempDir
+    lateinit var tempDir: Path
+
+    @Test
+    fun `if file type is not JSON or YAML (YML), exception is thrown`() {
+        val app = ZeroCodeSubCommand()
+        val filePath = Files.createFile(tempDir.resolve("src.txt"))
+//        tapSystemErrAndOutNormalized {
+//            CommandLine(app).execute("--file=$filePath")
+//        }.apply {
+//            Assertions.assertTrue(this.contains("Input file format not supported."))
+//        }
+        println(app)
+        println(filePath)
+    }
+}
